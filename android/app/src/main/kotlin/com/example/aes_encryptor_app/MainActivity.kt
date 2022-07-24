608b08f3966a2ca7d8f9053b7e314595988d432d
PeterAkande <akandepeter86@gmail.com>
2022-07-24 17:35:26 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/aes_encryptor_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aes_encryptor_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app_theme.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//Would change all these later.
+class ProductivityTheme {
+  static TextTheme lightTextTheme = TextTheme(
+    bodyText1: GoogleFonts.openSans(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.black,
+    ),
+    headline1: GoogleFonts.openSans(
+      fontSize: 29.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+    headline2: GoogleFonts.openSans(
+      fontSize: 21.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.black,
+    ),
+    headline3: GoogleFonts.openSans(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
+    headline6: GoogleFonts.openSans(
+      fontSize: 20.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
+  );
+
+  static TextTheme darkTextTheme = TextTheme(
+    bodyText1: GoogleFonts.openSans(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.white,
+    ),
+    headline1: GoogleFonts.openSans(
+      fontSize: 29.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+    headline2: GoogleFonts.openSans(
+      fontSize: 21.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.white,
+    ),
+    headline3: GoogleFonts.openSans(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.white,
+    ),
+    headline6: GoogleFonts.openSans(
+      fontSize: 20.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.white,
+    ),
+  );
+
+  static ThemeData light() {
+    return ThemeData(
+      primaryColor: const Color(0xFFF2E9E4),
+      brightness: Brightness.light,
+      checkboxTheme: CheckboxThemeData(
+        fillColor: MaterialStateColor.resolveWith((states) {
+          return Colors.black;
+        }),
+      ),
+      appBarTheme: const AppBarTheme(
+        //foregroundColor: Colors.white,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.black,
+      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        selectedItemColor: Colors.red,
+        // unselectedItemColor: Colors.white,
+        backgroundColor: Colors.white,
+      ),
+      textTheme: lightTextTheme,
+    );
+  }
+
+  static ThemeData dark() {
+    return ThemeData(
+      primaryColor: const Color(0x004a4e69),
+      brightness: Brightness.dark,
+      appBarTheme: AppBarTheme(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.grey[900],
+      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        selectedItemColor: Colors.green,
+      ),
+      textTheme: darkTextTheme,
+    );
+  }
+}

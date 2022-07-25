5c445a61b77b66fb22519aaa4c0c35d2ea31cdd3
PeterAkande <akandepeter86@gmail.com>
2022-07-25 09:32:29 +0100

    Added the colors that wuld be used

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/cupertino.dart';
 
-const buttonColor = Color(0xFFC9ADA7);
+//LightMode
+const lightModeStatusBarOverlay = Color(0xFFC9ADA7);
+const lightModeButtonSelected = Color(0xFF9A8C98);
+const lightModeButtonColor = Color(0xFFC9ADA7);
+const lightModeTextColor = Color(0xFF22223B);
+
+//DarkMode
+const darkModeStatusBarOverlay = Color(0xFFC9ADA7);
+const darkModeButtonSelected = Color(0xFFF2E9E4);
+const darkModeButtonColor = Color(0xC9ADA769);
+const darkModeTextColor = Color(0xFFF2E9E4);

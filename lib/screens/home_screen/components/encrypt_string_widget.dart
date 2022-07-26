cb962947ac784251b2c2851144152ca63475845e
PeterAkande <akandepeter86@gmail.com>
2022-07-26 02:17:13 +0100

    Made the Ui look better by adjusting the spaces between widgets

--- lib/screens/home_screen/components/encrypt_string_widget.dart
+++ lib/screens/home_screen/components/encrypt_string_widget.dart
@@ -2,7 +2,11 @@ import 'package:aes_encryptor_app/app_state/aes_encryptor_state.dart';
 import 'package:aes_encryptor_app/constants/app_colors.dart';
 import 'package:aes_encryptor_app/screens/home_screen/components/encrypt_button_.dart';
 import 'package:aes_encryptor_app/screens/home_screen/components/password_text_view.dart';
+import 'package:aes_encryptor_app/utils/size_config.dart';
+import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
 
 import '../../../utils/get_button_colors.dart';
@@ -49,7 +53,6 @@ class _EncryptStringWidgetState extends State<EncryptStringWidget> {
                     filled: true,
 
                     hintText: 'Enter Text to be encrypted',
-                    // contentPadding: EdgeInsets.only(left: 30, bottom: 9),
                     border: OutlineInputBorder(
                       borderSide: BorderSide.none,
                       borderRadius: BorderRadius.circular(10),
@@ -77,26 +80,28 @@ class _EncryptStringWidgetState extends State<EncryptStringWidget> {
         .isEmpty) {
       //if the
       return Column(
-        children: const [
+        children:  [
           SizedBox(
-            height: 120,
+            height: getProportionateScreenHeight(160),
           ),
           EncryptButton(),
         ],
       );
-    }else{
+    } else {
       return Column(
         children: [
           SizedBox(
-            height: 10,
+            height: getProportionateScreenHeight(20),
           ),
-          Container(
-            height: 180,
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(10)),
-            child: Column(
-              children: [
-                Expanded(
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Expanded(
+                child: Container(
+                  height: getProportionateScreenHeight(200),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10)),
                   child: TextField(
                     maxLines: null,
                     enabled: false,
@@ -124,9 +129,33 @@ class _EncryptStringWidgetState extends State<EncryptStringWidget> {
                     onSubmitted: (text) {},
                     onChanged: (text) {},
                   ),
-                )
-              ],
-            ),
+                ),
+              ),
+              const SizedBox(
+                width: 1,
+              ),
+              SizedBox(
+                // width: 30,
+                // height: 70,
+                child: IconButton(
+                  // splashRadius: 2,
+                  // iconSize: 60,
+                  icon: SvgPicture.asset(
+                    'assets/copy_icon.svg',
+                    height: 25,
+                  ),
+                  onPressed: () {
+                    FlutterClipboard.copy(context
+                            .read<AesEncryptorState>()
+                            .textDecryptedOrEncryptedController
+                            .text)
+                        .then((value) {
+                      Fluttertoast.showToast(msg: 'Copied');
+                    });
+                  },
+                ),
+              )
+            ],
           ),
         ],
       );
